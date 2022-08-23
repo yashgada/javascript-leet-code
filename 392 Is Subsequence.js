17a823da7be3a1204f622610dc4146a774a65fb5
Yash Gada <gadayash9@gmail.com>
2022-08-23 14:01:58 +0530

    More solutions for 392

--- 392 Is Subsequence.js
+++ 392 Is Subsequence.js
@@ -17,5 +17,33 @@ const isSubsequence = (s, t) => {
   return false;
 };
 
-// console.log(isSubsequence("abc", "ahbgdc"));
-console.log(isSubsequence("", ""));
+// solution using two pointers
+const isSubsequence_ForLoop = (s, t) => {
+  let p1 = 0;
+  for (let i = 0; i < t.length, p1 < s.length; i++) {
+    if (s[p1] === t[i]) {
+      p1++;
+    }
+  }
+  if (p1 === s.length) {
+    return true;
+  }
+  return false;
+};
+
+const isSubsequence_TwoPointers = (s, t) => {
+  let p1 = 0,
+    p2 = 0;
+  while (p1 < s.length && p2 < t.length) {
+    if (s[p1] === t[p2]) {
+      p1++;
+      p2++;
+    } else {
+      p2++;
+    }
+  }
+  return p1 === s.length;
+};
+
+console.log(isSubsequence_TwoPointers("abc", "ahbgdc"));
+// console.log(isSubsequence_TwoPointers("", ""));
