4e3f5db5df2cf2cc193c67868e19a310a405e892
Yash Gada <gadayash9@gmail.com>
2022-09-04 00:37:33 +0530

    Done with 509 Fibonacci Number

---
+++ 509 Fibonacci Number.js
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/fibonacci-number/
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function (n) {
+  if (n === undefined) return;
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  return fib(n - 1) + fib(n - 2);
+};
+
+console.log(fib(8));
