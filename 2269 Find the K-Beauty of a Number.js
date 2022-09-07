2674046c2d0c1b3a6e9095609abca8a6578c3ca2
Yash Gada <gadayash9@gmail.com>
2022-09-07 22:59:53 +0530

    Done with 2269 Find the K-Beauty of a Number

---
+++ 2269 Find the K-Beauty of a Number.js
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/find-the-k-beauty-of-a-number/
+
+/**
+ * @param {number} num
+ * @param {number} k
+ * @return {number}
+ */
+var divisorSubstrings = function (num, k) {};
